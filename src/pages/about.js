811aaa74ac199a43a491490251cab49b22af350d
disharjayantha <dishuj15@gmail.com>
2021-01-10 21:52:02 +0530

    1st gatsby example

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About gatsby" />
+      <Header headerText="it's pretty cool" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
